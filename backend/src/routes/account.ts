9bc0a6b5441469e49058cd4f4d6be0f459c5373f
Rishabh Damle <dwrushabh@gmail.com>
2026-01-14 14:47:40 +0530

    added check balance endpoint

--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -1,4 +1,16 @@
 import express from "express";
 import Router from "express";
-const AccountRounter = Router();
-AccountRounter.use(express.json());
+import type { IGetUserAuthInfoRequest } from "../types/express/index.js";
+import { AccountsModel } from "../db.js";
+export const accountRounter = Router();
+accountRounter.use(express.json());
+accountRounter.get("/balance", async (req: IGetUserAuthInfoRequest, res) => {
+  const userId = req.userId;
+  const account = await AccountsModel.findOne({
+    userId,
+  });
+
+  res.status(200).json({
+    Balance: account?.balance,
+  });
+});
