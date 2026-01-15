240635a4b14b1915378b0d21a6f1de9e9545ce45
Rishabh Damle <dwrushabh@gmail.com>
2026-01-15 15:18:41 +0530

    Added transaction route with the implementation of transaction in mongoDB

--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -2,15 +2,79 @@ import express from "express";
 import Router from "express";
 import type { IGetUserAuthInfoRequest } from "../types/express/index.js";
 import { AccountsModel } from "../db.js";
+import { userAuth } from "../middlwares/userAuth.js";
+import { startSession } from "mongoose";
 export const accountRounter = Router();
 accountRounter.use(express.json());
-accountRounter.get("/balance", async (req: IGetUserAuthInfoRequest, res) => {
-  const userId = req.userId;
-  const account = await AccountsModel.findOne({
-    userId,
-  });
+accountRounter.get(
+  "/balance",
+  userAuth,
+  async (req: IGetUserAuthInfoRequest, res) => {
+    const userId = req.userId;
+    const account = await AccountsModel.findOne({
+      userId,
+    });
 
-  res.status(200).json({
-    Balance: account?.balance,
-  });
-});
+    res.status(200).json({
+      Balance: account?.balance,
+    });
+  }
+);
+
+accountRounter.post(
+  "/transfer",
+  userAuth,
+  async (req: IGetUserAuthInfoRequest, res) => {
+    const session = await startSession();
+    session.startTransaction();
+    const senderUserId = req.userId;
+    const { reciverUserId, amount } = req.body;
+    const senderAccount = await AccountsModel.findOne({
+      senderUserId,
+    });
+    if (!senderAccount) {
+      return res.status(400).json({ message: "Invalid sender account" });
+    }
+    const balance = senderAccount.balance ?? 0;
+    if (balance < amount) {
+      res.status(400).json({ message: "Insufficient balance" });
+    }
+
+    const reciverAccount = await AccountsModel.findOne({
+      reciverUserId,
+    });
+
+    if (!reciverAccount) {
+      res.status(400).json({
+        message: "Invalid reciver account",
+      });
+    }
+
+    await AccountsModel.findByIdAndUpdate(
+      {
+        senderUserId,
+      },
+      {
+        $inc: {
+          balance: -amount,
+        },
+      }
+    );
+
+    await AccountsModel.findByIdAndUpdate(
+      {
+        reciverUserId,
+      },
+      {
+        $inc: {
+          balance: amount,
+        },
+      }
+    );
+
+    await session.commitTransaction();
+    res.status(200).json({
+      Message: "Transfer successful",
+    });
+  }
+);
